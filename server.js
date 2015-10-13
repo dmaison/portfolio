4e70abbb70dd3a9d721335adf39675538bb77f5f
David J Maison Jr <davem@interlinkone.com>
2015-10-13 18:31:49 -0400

    remove reference to unused package

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express 		= require( 'express' );
 var app 			= express();
 var bodyParser 		= require( 'body-parser' );
 var methodOverride 	= require( 'method-override' );
-var mongoose 		= require( 'mongoose' );
 var session 		= require( 'express-session' );
 var cookieParser 	= require( 'cookie-parser' );
 var clc 			= require('cli-color');
