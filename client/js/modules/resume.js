f927e03f07fe166104d490046b756b47f49a03a4
Dave Maison <davem@interlinkone.com>
2016-10-28 00:40:47 +0000

    standardization

--- client/js/modules/resume.js
+++ client/js/modules/resume.js
@@ -1,59 +1,66 @@
 angular
 	.module( 'app.resume', [] )
 	.config( [ '$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ){
-
+		
 		$locationProvider.html5Mode( true );
-
+		
 		$routeProvider
 			.when( '/resume', {
 				templateUrl	: 'views/resume.html',
 				controller 	: 'resume',
 				reloadOnSearch: false
 			});
-
+		
 	}])
-	.controller( 'resume', [ '$scope', '$http', '$window', '$location', function( $scope, $http, $window, $location ) {
-
-		$scope.page = ( $location.hash() != '' ) ? $location.hash() : 'about';
-
+	.controller( 'resume', [ '$scope', '$http', '$rootScope', '$location', function( $scope, $http, $rootScope, $location ) {
+		
+		/* global angular */
+		
+		$scope.page 	= ( $location.hash() != '' ) ? $location.hash() : 'about';
+		$scope.loading	= true;
+		
 		$scope.navigate = function( page ){
 			$scope.page = page;
 			$location.hash( page );
-		}
-
+		};
+		
 		$scope.$watch( 'page', function( value ){
-
+			
 			switch( value ){
-
+				
 				case 'expertise':
 					if( $scope.languages ) break;
-					$scope.loading = true;
+					
 					$http
 						.get( '/api/qualifications/' )
-						.success(function( res ){
-							$scope.languages = res;
+						.then(function( res ){
+							$scope.languages = res.data;
+							$scope.loading = false;
+						}, function( err ){
+							$rootScope.error = err.data.message;
 							$scope.loading = false;
 						});
 					break;
-
+					
 				case 'experience':
 					if( $scope.jobs ) break;
 					$scope.loading = true;
 					$http
 						.get( '/api/experience/' )
-						.success(function( res ){
-							$scope.jobs = res;
+						.then(function( res ){
+							$scope.jobs = res.data;
+							$scope.loading = false;
+						}, function( err ){
+							$rootScope.error = err.data.message;
 							$scope.loading = false;
 						});
 					break;
-
+					
 				default:
 					$scope.loading = false;
 					break;
 			}
-
+			
 		});
-
 		
-
 	}]);
