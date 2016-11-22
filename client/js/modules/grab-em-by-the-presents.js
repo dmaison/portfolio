56e2034d0076cf059c58e84580941d18e586923c
Dave Maison <davem@interlinkone.com>
2016-11-22 02:29:32 +0000

    sort

--- client/js/modules/grab-em-by-the-presents.js
+++ client/js/modules/grab-em-by-the-presents.js
@@ -20,7 +20,10 @@ angular
 			.get( '/api/presents' )
 			.then(function( res ){
 				$rootScope.loading 	= false;
-				$scope.pairings		= res.data;
+				$scope.pairings		= res.data.sort(function( a, b ){
+					if( a.buyer > b.buyer ) return 1;
+					if( a.buyer < b.buyer ) return -1;
+				});
 			},function( err ){
 				$rootScope.error	= err.data.message;
 			});
