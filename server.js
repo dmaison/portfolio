ef01ecf690b39a6cbfaa7fd3f6e6e6bddb349088
Dave Maison <davem@interlinkone.com>
2017-01-03 02:43:11 +0000

    cleanup server

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-/* MODULES -------------------- */
 var express 		= require( 'express' );
-var app 			= express();
 var bodyParser 		= require( 'body-parser' );
 var methodOverride 	= require( 'method-override' );
-var session 		= require( 'express-session' );
-var cookieParser 	= require( 'cookie-parser' );
-var clc 			= require('cli-color');
-
-// set app port
+var clc 			= require( 'cli-color' );
 var port 			= process.env.PORT || 8081;
+var app 			= express();
 
-// parse request objects
 app.use( bodyParser.json() );
-
-// set content type of server response
 app.use( bodyParser.json({ type: 'application/vnd.api+json' }) );
-
-// parse application/x-www-form-urlencoded (???)
 app.use( bodyParser.urlencoded({ extended: true }) );
-
-// set default methods: get, post, put, delete
 app.use( methodOverride( 'X-HTTP-Method-Override' ) );
-
-// set base directory for client side resources
 app.use( express.static(__dirname + '/client') ); 
-
-/* ROUTES -------------------- */
-
-// set routes
 app.use( '/api', require( './server/router.api' ) );
 app.use( '/', require( './server/router' ) );
 
-/* START APP -------------------- */
 app.listen( port );
 console.log( 'Server running on port ' + clc.greenBright( port ) );
 
-// expose app
 exports = module.exports = app;
