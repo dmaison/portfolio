5af37de5a89e989a63ce25b9d0fca8b9282ce79f
Dave Maison <davem@interlinkone.com>
2017-01-04 22:13:57 +0000

    refactor paginate

--- client/js/directives/pagination.js
+++ client/js/directives/pagination.js
@@ -1,68 +1,100 @@
 /* global angular */
 
-(function() {
-    'use strict';
-	    
+(function(){
+	'use strict';
+	
 	angular
-		.module( 'pagination', [])
-		.directive( 'paginate', directivePaginate );
+		.module( 'Paginate', [])
+		.directive( 'paginate', directive );
 		
-	function directivePaginate() {
+	function directive() {
 		return {
 			restrict: 'E',
 			templateUrl: 'js/directives/templates/pagination.html',
 			scope: {
 				list: 			'=',
-				itemsPerPage: 	'='
+				itemsPerPage: 	'=',
+				filter: 		'=',
+				filterOnProperty:'=',
+				ignoreSort: 	'='
 			},
-			controller: [ '$scope', '$interval', controllerPaginate ]
+			controller: [ '$scope', '$interval', 'filterFilter', controller ]
 	  	};
 	}
-	
-	function controllerPaginate( $scope, $interval ){
-				
-		var checkList = $interval(function(){
-			if( !$scope.list ) return;
-			$scope.currentPage 	= 0;
-			$scope.total 		= $scope.list.length;
-			$scope.pagedList	= $scope.list;
-			$scope.list 		= $scope.pagedList.slice( 0, $scope.itemsPerPage );
-		}, 200);				
 		
-		$scope.range = function() {
-		    var rangeSize 	= Math.ceil( $scope.total/ $scope.itemsPerPage );
-		    var ret 		= [];
-		    var start 		= $scope.currentPage;
-		    
-		    if( start > $scope.pageCount()-rangeSize ) start = $scope.pageCount() - rangeSize;
+	function controller( $scope, $interval, filterFilter ){
 		
-		    for( var i = start; i < ( start + rangeSize ); i++ ) ret.push( i );
-		    
-		    return ret;
-		};
+		$scope.pageCount	= count;
+		var checkList		= $interval( checkInterval, 200, 20, true, $scope );
 		
 		$scope.prevPage = function(){
-		    if( $scope.currentPage > 0 ) $scope.currentPage--;
-	  	};
+			if( $scope.currentPage > 0 ) $scope.currentPage--;
+		};
 		
-	  	$scope.nextPage = function(){
-		    if( $scope.currentPage < $scope.pageCount() - 1 ) $scope.currentPage++;
-	  	};
-		
-	  	$scope.pageCount = function(){
-		    return Math.ceil( $scope.total/$scope.itemsPerPage );
-	  	};
+		$scope.nextPage = function(){
+			if( $scope.currentPage < count( $scope.total, $scope.itemsPerPage ) - 1 ) $scope.currentPage++;
+		};
 		
 		$scope.setPage = function( page ){
-		    if( page > -1 && page < $scope.pageCount() ) $scope.currentPage = page;
-	  	};
+			if( page > -1 && page < count( $scope.total, $scope.itemsPerPage ) ) $scope.currentPage = page;
+		};
+		
+		$scope.$watch( 'filter', function(){
+			if( !$scope.list || !$scope.pagedList ) return;
+			var list 		= filter( $scope.pagedList, $scope.filter, $scope.filterOnProperty, filterFilter );
+			$scope.total 	= list.length;
+			$scope.list 	= list.slice( $scope.currentPage, $scope.itemsPerPage );
+			$scope.range 	= calculateRange( $scope.total, $scope.itemsPerPage );
+			$scope.setPage( 0 );
+		});
 		
 		$scope.$watch( 'currentPage', function( newValue, oldValue ){
-			if( !$scope.list ) return;
+			if( !$scope.list || !$scope.pagedList ) return;
 			$interval.cancel( checkList );
 			var offset 		= ( newValue * $scope.itemsPerPage );
-		    $scope.list 	= $scope.pagedList.slice( offset, offset + $scope.itemsPerPage );
-	  	});
+			var list 		= filter( $scope.pagedList, $scope.filter, $scope.filterOnProperty, filterFilter );
+			$scope.total 	= list.length;
+			$scope.list 	= list.slice( offset, offset + $scope.itemsPerPage );
+		});
+		
+	}
+	
+	function checkInterval( $scope ){
+		if( !$scope.list ) return;
+		$scope.currentPage 	= 0;
+		$scope.pagedList 	= [];
+		$scope.total 		= $scope.list.length;
+		angular.copy( $scope.list, $scope.pagedList );
+		$scope.list 		= $scope.pagedList.slice( 0, $scope.itemsPerPage );
+		$scope.range 		= calculateRange( $scope.total, $scope.itemsPerPage );
+	}
+	
+	function filter( lists, searchTerm, filterOnProperty, filterFilter ){
+		if (!searchTerm || (searchTerm.length == 0)) return lists;
+		if (!filterOnProperty || (filterOnProperty.length == 0)) return filterFilter( lists, searchTerm );
+		
+		var pattern = new RegExp(searchTerm, "i");
+		var result = [];
+		angular.forEach( lists, function( item ){
+			filterOnProperty.forEach(function( prop ){
+				if( item[ prop ].search( pattern ) > -1 ) return result.push( item );
+			});
+		});
+		return result;
+	}
+	
+	function count( total, limit ){
+	    return Math.ceil( total / limit );
+  	}
+	
+	function calculateRange( total, limit ){
+		var rangeSize 	= count( total, limit );
+		var ret 		= [];
+		var start 		= 0;
+		
+		for( var i = start; i < ( start + rangeSize ); i++ ) ret.push( i );
+		
+		return ret;
 	}
 	
 })();
