8f4e159df2dff09c222f83ec767a91095bc989c9
Dave Maison <davemaison@gmail.com>
2020-01-28 18:38:54 -0500

    first page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Page from '../../components/Page'
 import './style.css'
 
-const Home = props => (
+const Home = () => (
     <Page id="home">
         <h1>
             dave.maison
             <small>web developer | veteran | amateur strongman</small>
         </h1>
+        <i className="fas fa-angle-double-down fa-2x" />
     </Page>
 );
 
