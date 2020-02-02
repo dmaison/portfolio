9c578bd5dd35cb621c62a45f6a61a22f58cde22e
Dave Maison <davemaison@gmail.com>
2020-02-02 14:26:56 -0500

    update

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { InView } from 'react-intersection-observer'
 import Page from '../../components/Page'
 import Job from '../../components/Job'
 import Masonry from 'react-masonry-component'
@@ -7,17 +8,29 @@ import './style.css'
 
 function Resume( props ){
     
+    const [ visible, setVisible ] = useState( false );
+
+    useEffect(() => {
+        console.log( 'test' )
+    })
+
+    const handleVisible = ( inView, entry ) => {
+        console.log( inView, entry );
+    }
+
     return (
-        <Page id="resume">
-            <Page.Container>
-                <h2>Resume</h2>
-                <Masonry className="jobs">
-                    {
-                        ResumeData.history.map( ( job, index ) => <Job { ...job } key={ index } /> )
-                    }
-                </Masonry>
-            </Page.Container>
-        </Page>
+        <InView onChange={ handleVisible }>
+            <Page id="resume">
+                <Page.Container>
+                    <h2>Resume</h2>
+                    <Masonry className="jobs">
+                        {
+                            ResumeData.history.map( ( job, index ) => <Job { ...job } key={ index } /> )
+                        }
+                    </Masonry>
+                </Page.Container>
+            </Page>
+        </InView>
     )
 
 }
