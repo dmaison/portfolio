330bf18b0056e95b75880999d231cb9e657c9c7a
dmaison <davemaison@gmail.com>
2020-11-09 16:35:46 -0500

    fix logo

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Icon from './dave.maison.svg'
 import './style.css'
 
 const Component = () => (
     <h1 className="title">
-        <NavLink to="/">dave.maison</NavLink>
+        <NavLink to="/">
+            <img src={ Icon } alt="dave.maison" />
+        </NavLink>
     </h1>
 )
 
