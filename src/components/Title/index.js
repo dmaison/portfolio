de2faa8bf6ca2cf81cfffae866d676d4c65ecf90
dmaison <davemaison@gmail.com>
2020-11-11 15:35:10 -0500

    main mobile design finish

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Icon from './dave.maison.svg'
+import { useMediaQuery } from 'react-responsive'
+import IconVertical from './dave.maison-vertical.svg'
+import IconHorizontal from './dave.maison-horizontal.svg'
 import './style.css'
 
-const Component = () => (
-    <h1 className="title">
-        <NavLink to="/">
-            <img src={ Icon } alt="dave.maison" />
-        </NavLink>
-    </h1>
-)
+const Component = () => {
+
+    const isMobile = useMediaQuery({ maxWidth: 750 });
+
+    return (
+        <h1 className="title">
+            <NavLink to="/">
+                <img src={ isMobile ? IconHorizontal : IconVertical } alt="dave.maison" />
+            </NavLink>
+        </h1>
+    )
+}
 
 export default Component
