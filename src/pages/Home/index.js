17daf247dbb246261134c8eaa79244122639e9e4
dmaison <davemaison@gmail.com>
2020-11-12 09:24:28 -0500

    add quotes to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import Quotes from '../../data/quotes.json'
 import './style.css'
 
-const Component = () => (
-    <section className="page-home">
-        <h2>Welcome</h2>
-    </section>
-)
+const Component = () => {
+
+    const index = Math.floor( Math.random() * Quotes.length ),
+    quote = Quotes[ index ];
+
+    return (
+        <section className="page-home">
+            <h2>Welcome</h2>
+            <blockquote className="page-home__quote">
+                { quote.quote }
+                <footer className="page-home__quote__quotee">- { quote.quotee }</footer>
+            </blockquote>
+        </section>
+    )
+}
 
 export default Component
