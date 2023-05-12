89b6fb9c584dc28a0b25fedb9f1977682d65419d
Maison <David.Maison@analog.com>
2023-05-12 13:40:42 -0400

    make the scroll bar themed

--- src/router/Layout/theme.js
+++ src/router/Layout/theme.js
@@ -24,4 +24,33 @@ const theme = createTheme({
     }
 });
 
+theme.components.MuiCssBaseline = {
+    styleOverrides: {
+        body: {
+            scrollbarColor: theme.palette.primary.main,
+            "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+                backgroundColor: theme.palette.grey[ 900 ],
+            },
+            "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+                borderRadius: 8,
+                backgroundColor: theme.palette.grey[ 700 ],
+                minHeight: 24,
+                border: `3px solid ${ theme.palette.grey[ 900 ] }`
+            },
+            "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
+                backgroundColor: theme.palette.primary.main,
+            },
+            "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
+                backgroundColor: theme.palette.primary.main,
+            },
+            "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+                backgroundColor: theme.palette.primary.main,
+            },
+            "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+                backgroundColor: theme.palette.primary.main,
+            },
+        }
+    }
+};
+
 export default responsiveFontSizes( theme );
