ff7df79284d44a7e90a2d0a5566120a282d2980c
dmaison <davemaison@gmail.com>
2024-10-16 23:57:23 -0400

    obnoxious lol

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,18 @@ import { getQuadrant } from './window';
 
 const 
 
+handleHashChange = () => {
+    const className = 'active',
+    { hash } = window.location,
+    current = document.querySelector( `nav a.${className}` ),
+    next = document.querySelector( `nav a[href="${hash}"]` ),
+    color = hash.replace( '#', '' ) || 'none';
+    document.documentElement.style.setProperty( '--c-accent-100', `var( --c-${color} )` );
+
+    current?.classList.remove( className );
+    next?.classList.add( className );
+},
+
 handleMouseMove = ( header, e ) => { 
     const { pageX: x, pageY: y } = e,
     [ shadowX, shadowY ] = getQuadrant( x, y );
@@ -15,6 +27,9 @@ handleMouseMove = ( header, e ) => {
 init = () => {
     const header = document.querySelector( 'header' );
     document.addEventListener( 'mousemove', handleMouseMove.bind( null, header ), true );
+    handleHashChange();
 }
 
 DOMReady( init );
+
+window.addEventListener( 'hashchange', handleHashChange, false );
